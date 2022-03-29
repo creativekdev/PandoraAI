1957e63ba9a63125587d251e8d90bfb52d21d207
Jinchun Xia <jinchun@boostinsider.com>
2022-03-29 16:28:54 +0800

    .

--- lib/Ui/HomeScreen.dart
+++ lib/Ui/HomeScreen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cartoonizer/Common/importFile.dart';
@@ -108,18 +107,18 @@ class _HomeScreenState extends State<HomeScreen> {
                         itemCount: (snapshot.data as List<EffectModel>).length,
                         itemBuilder: (context, index) => GestureDetector(
                           onTap: () => {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  settings: RouteSettings(name: "/PurchaseScreen"),
-                                  builder: (context) => PurchaseScreen(),
-                                ))
                             // Navigator.push(
                             //     context,
                             //     MaterialPageRoute(
-                            //       settings: RouteSettings(name: "/ChoosePhotoScreen"),
-                            //       builder: (context) => ChoosePhotoScreen(list: (snapshot.data as List<EffectModel>), pos: index),
+                            //       settings: RouteSettings(name: "/PurchaseScreen"),
+                            //       builder: (context) => PurchaseScreen(),
                             //     ))
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  settings: RouteSettings(name: "/ChoosePhotoScreen"),
+                                  builder: (context) => ChoosePhotoScreen(list: (snapshot.data as List<EffectModel>), pos: index),
+                                ))
                           },
                           child: Container(
                             margin: EdgeInsets.only(left: 5.w, right: 5.w, bottom: 2.h),
