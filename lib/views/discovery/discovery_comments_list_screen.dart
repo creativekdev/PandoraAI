a71a4ea73603a9bc59c5dd458f6ed57237fc08a4
wangyu <wangyu@socialbook.io>
2022-07-11 13:06:13 +0800

    1. fix bugs: list refresh after create comment

--- lib/views/discovery/discovery_comments_list_screen.dart
+++ lib/views/discovery/discovery_comments_list_screen.dart
@@ -173,7 +173,7 @@ class DiscoveryCommentsListState extends AppState<DiscoveryCommentsListScreen> {
     await hideLoading();
     if (baseEntity != null) {
       CommonExtension().showToast('Comment posted');
-      _refreshController.callRefresh();
+      delay(() => _refreshController.callRefresh());
     }
     return baseEntity != null;
   }
--- lib/views/discovery/discovery_secondary_comment_list_screen.dart
+++ lib/views/discovery/discovery_secondary_comment_list_screen.dart
@@ -156,7 +156,7 @@ class DiscoverySecondaryCommentsListState extends AppState<DiscoverySecondaryCom
     await hideLoading();
     if (baseEntity != null) {
       CommonExtension().showToast('Comment posted');
-      _refreshController.callRefresh();
+      delay(() => _refreshController.callRefresh());
     }
     return baseEntity != null;
   }
