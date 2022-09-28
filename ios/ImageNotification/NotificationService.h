b983459d1ae22509c1ae9f5b714bcd3e882e6e73
wangyu <wangyu@socialbook.io>
2022-09-28 17:43:19 +0800

    1. handle image notification in ios platform

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by 剑雨游魂 on 2022/9/28.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
