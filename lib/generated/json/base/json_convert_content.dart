f5a75fafe606245ae41c4e1df4a42f4d22359199
wangyu <wangyu@socialbook.io>
2023-06-06 17:50:10 +0800

    1. add shipping_method parsed

--- lib/generated/json/base/json_convert_content.dart
+++ lib/generated/json/base/json_convert_content.dart
@@ -25,6 +25,7 @@ import 'package:cartoonizer/models/platform_connection_entity.dart';
 import 'package:cartoonizer/models/push_extra_entity.dart';
 import 'package:cartoonizer/models/rate_config_entity.dart';
 import 'package:cartoonizer/models/recent_entity.dart';
+import 'package:cartoonizer/models/shipping_method_entity.dart';
 import 'package:cartoonizer/models/txt2img_result_entity.dart';
 import 'package:cartoonizer/models/txt2img_style_entity.dart';
 import 'package:cartoonizer/models/upload_record_entity.dart';
@@ -71,6 +72,9 @@ class JsonConvert {
 		(RecentEffectItem).toString(): RecentEffectItem.fromJson,
 		(RecentMetaverseEntity).toString(): RecentMetaverseEntity.fromJson,
 		(RecentGroundEntity).toString(): RecentGroundEntity.fromJson,
+		(ShippingMethodEntity).toString(): ShippingMethodEntity.fromJson,
+		(ShippingMethodShippingRateData).toString(): ShippingMethodShippingRateData.fromJson,
+		(ShippingMethodShippingRateDataFixedAmount).toString(): ShippingMethodShippingRateDataFixedAmount.fromJson,
 		(Txt2imgResultEntity).toString(): Txt2imgResultEntity.fromJson,
 		(Txt2imgStyleEntity).toString(): Txt2imgStyleEntity.fromJson,
 		(UploadRecordEntity).toString(): UploadRecordEntity.fromJson,
@@ -257,6 +261,15 @@ List<T>? convertListNotNull<T>(dynamic value, {EnumConvertFunction? enumConvert}
 		}
 		if(<RecentGroundEntity>[] is M){
 			return data.map<RecentGroundEntity>((Map<String, dynamic> e) => RecentGroundEntity.fromJson(e)).toList() as M;
+		}
+		if(<ShippingMethodEntity>[] is M){
+			return data.map<ShippingMethodEntity>((Map<String, dynamic> e) => ShippingMethodEntity.fromJson(e)).toList() as M;
+		}
+		if(<ShippingMethodShippingRateData>[] is M){
+			return data.map<ShippingMethodShippingRateData>((Map<String, dynamic> e) => ShippingMethodShippingRateData.fromJson(e)).toList() as M;
+		}
+		if(<ShippingMethodShippingRateDataFixedAmount>[] is M){
+			return data.map<ShippingMethodShippingRateDataFixedAmount>((Map<String, dynamic> e) => ShippingMethodShippingRateDataFixedAmount.fromJson(e)).toList() as M;
 		}
 		if(<Txt2imgResultEntity>[] is M){
 			return data.map<Txt2imgResultEntity>((Map<String, dynamic> e) => Txt2imgResultEntity.fromJson(e)).toList() as M;
---
+++ lib/generated/json/shipping_method_entity.g.dart
@@ -0,0 +1,62 @@
+import 'package:cartoonizer/generated/json/base/json_convert_content.dart';
+import 'package:cartoonizer/models/shipping_method_entity.dart';
+
+ShippingMethodEntity $ShippingMethodEntityFromJson(Map<String, dynamic> json) {
+	final ShippingMethodEntity shippingMethodEntity = ShippingMethodEntity();
+	final ShippingMethodShippingRateData? shippingRateData = jsonConvert.convert<ShippingMethodShippingRateData>(json['shipping_rate_data']);
+	if (shippingRateData != null) {
+		shippingMethodEntity.shippingRateData = shippingRateData;
+	}
+	return shippingMethodEntity;
+}
+
+Map<String, dynamic> $ShippingMethodEntityToJson(ShippingMethodEntity entity) {
+	final Map<String, dynamic> data = <String, dynamic>{};
+	data['shipping_rate_data'] = entity.shippingRateData.toJson();
+	return data;
+}
+
+ShippingMethodShippingRateData $ShippingMethodShippingRateDataFromJson(Map<String, dynamic> json) {
+	final ShippingMethodShippingRateData shippingMethodShippingRateData = ShippingMethodShippingRateData();
+	final String? type = jsonConvert.convert<String>(json['type']);
+	if (type != null) {
+		shippingMethodShippingRateData.type = type;
+	}
+	final ShippingMethodShippingRateDataFixedAmount? fixedAmount = jsonConvert.convert<ShippingMethodShippingRateDataFixedAmount>(json['fixed_amount']);
+	if (fixedAmount != null) {
+		shippingMethodShippingRateData.fixedAmount = fixedAmount;
+	}
+	final String? displayName = jsonConvert.convert<String>(json['display_name']);
+	if (displayName != null) {
+		shippingMethodShippingRateData.displayName = displayName;
+	}
+	return shippingMethodShippingRateData;
+}
+
+Map<String, dynamic> $ShippingMethodShippingRateDataToJson(ShippingMethodShippingRateData entity) {
+	final Map<String, dynamic> data = <String, dynamic>{};
+	data['type'] = entity.type;
+	data['fixed_amount'] = entity.fixedAmount.toJson();
+	data['display_name'] = entity.displayName;
+	return data;
+}
+
+ShippingMethodShippingRateDataFixedAmount $ShippingMethodShippingRateDataFixedAmountFromJson(Map<String, dynamic> json) {
+	final ShippingMethodShippingRateDataFixedAmount shippingMethodShippingRateDataFixedAmount = ShippingMethodShippingRateDataFixedAmount();
+	final int? amount = jsonConvert.convert<int>(json['amount']);
+	if (amount != null) {
+		shippingMethodShippingRateDataFixedAmount.amount = amount;
+	}
+	final String? currency = jsonConvert.convert<String>(json['currency']);
+	if (currency != null) {
+		shippingMethodShippingRateDataFixedAmount.currency = currency;
+	}
+	return shippingMethodShippingRateDataFixedAmount;
+}
+
+Map<String, dynamic> $ShippingMethodShippingRateDataFixedAmountToJson(ShippingMethodShippingRateDataFixedAmount entity) {
+	final Map<String, dynamic> data = <String, dynamic>{};
+	data['amount'] = entity.amount;
+	data['currency'] = entity.currency;
+	return data;
+}
