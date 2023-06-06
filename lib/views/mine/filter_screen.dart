f7c16e7241cc428b983d242627f5d0e8838cb61d
creativekdev <creativekdev@gmail.com>
2023-06-06 06:37:03 -0400

    implemented filters

--- lib/views/mine/filter_screen.dart
+++ lib/views/mine/filter_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:cartoonizer/Widgets/state/app_state.dart';
+import 'package:cartoonizer/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:opencv_4/factory/pathfrom.dart';
@@ -10,6 +11,7 @@ import 'package:opencv_4/opencv_4.dart';
 //uncomment when image_picker is installed
 import 'package:image_picker/image_picker.dart';
 import 'package:sizer/sizer.dart';
+import 'package:image/image.dart' as imgLib;
 
 class FilterScreen extends StatefulWidget {
   const FilterScreen({Key? key}) : super(key: key);
@@ -19,7 +21,8 @@ class FilterScreen extends StatefulWidget {
 }
 
 class _FilterScreenState extends AppState<FilterScreen> {
-  File? _image;
+  File? _imagefile;
+  late imgLib.Image _image;
   late Uint8List _imageData;
   Uint8List? _byte;
   String _versionOpenCV = 'OpenCV';
@@ -28,8 +31,6 @@ class _FilterScreenState extends AppState<FilterScreen> {
     "normal",
     "discolor",
     "invert",
-    "dodgeColor",
-    "gaussBlur",
     "EdgeDetection",
     "Sharpen",
     "OldTimeFilter",
@@ -87,53 +88,205 @@ class _FilterScreenState extends AppState<FilterScreen> {
 
   _setURL() async {
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
-    _image = File(pickedFile!.path);
-    final ByteData data = await rootBundle.load(pickedFile.path);
+    _imagefile = File(pickedFile!.path);
+//    final ByteData data = await rootBundle.load(pickedFile.path);
+    _image = await getLibImage(await getImage(_imagefile!));
+
     setState(() {
-      _imageData = data.buffer.asUint8List();
-      _byte = data.buffer.asUint8List();
+      _imagefile;
+//      _imageData = data.buffer.asUint8List();
+//      _image = MemoryImage(_imageData);
+//      _byte = _image;
     });
+  }
+  int getR(int pixel) {
+    return  pixel & 0xFF;
+  }
+  int getG(int pixel) {
+    return  (pixel >> 8) & 0xFF;
+  }
+  int getB(int pixel) {
+    return  (pixel >> 16) & 0xFF;
+  }
+  setRGB(int pixel, int r, int g, int b){
+    if(r > 255) r= 255;
+    else if(r < 0) r = 0;
+    if(g > 255) g= 255;
+    else if(g < 0) g = 0;
+    if(b > 255) b= 255;
+    else if(b < 0) b = 0;
+
+    return (pixel & 0xFF000000) | ((b << 16) & 0x00FF0000) | ((g <<
+        8) & 0x0000FF00) | ((r) & 0x000000FF);
+  }
+  //This is spacial filter, not so good, should use FFT and IFT to speed up.
+  convolution(imgLib.Image image, List<int> kernel)
+  {
+    List<int> di = [-1, 0, 1, -1, 0, 1, -1, 0, 1];
+    List<int> dj = [-1, -1, -1, 0, 0, 0, 1, 1, 1];
+    imgLib.Image  __image = imgLib.copyCrop(image, 0, 0, image.width, image.height);
+
+    for (int i = 1; i < image.width - 1; i++) {
+      for (int j = 1; j < image.height - 1; j++) {
+        int valr, valb, valg;
+        valr = valb = valg = 0;
+        for (int c = 0; c < 9; c ++) {
+          int pixel = image.getPixel(i + di[c], j + dj[c]);
+          valr = valr + getR(pixel) * kernel[c];
+          valg = valg + getG(pixel) * kernel[c];
+          valb = valb + getB(pixel) * kernel[c];
+        }
+        __image.setPixel(i, j, setRGB(image.getPixel(i, j), valr, valg, valb));
+      }
+    }
+    return __image;
   }
 
   _Filter(String filter) async {
     //uncomment when image_picker is installed
-    if (_image != null) {
+    if (_imagefile != null) {
       try {
         //test with threshold
         switch (filter) {
           case "normal":
-            _byte = _imageData;
+            _byte = Uint8List.fromList(imgLib.encodeJpg(_image));
             break;
           case "discolor":
             _byte = await Cv2.cvtColor(
                 pathFrom: CVPathFrom.GALLERY_CAMERA,
-                pathString: _image!.path,
+                pathString: _imagefile!.path,
                 outputType: Cv2.COLOR_BGR2GRAY);
             break;
           case "invert":
-            break;
-          case "dodgeColor":
+            imgLib.Image  __image = imgLib.copyCrop(_image, 0, 0, _image.width, _image.height);
+            for (int i = 0; i < __image.width; i++) {
+              for (int j = 0; j < __image.height; j++) {
+                var pixel = __image.getPixel(i, j);
+                int r = getR(pixel);
+                int g = getG(pixel);
+                int b = getB(pixel);
+                r = 255 - r;
+                g = 255 - g;
+                b = 255 - b;
+                pixel = setRGB(pixel, r, g, b);
+                __image.setPixel(i, j, pixel);
+              }
+            }
+            _byte = Uint8List.fromList(imgLib.encodeJpg(__image));
             break;
           case "EdgeDetection":
-            break;
-
-          case "gaussBlur":
-            _byte = await Cv2.gaussianBlur(
+            _byte = await Cv2.laplacian(
                 pathFrom: CVPathFrom.GALLERY_CAMERA,
-                pathString: _image!.path,
-                kernelSize: [6, 6],
-                sigmaX: 2);
+                pathString: _imagefile!.path,
+                depth:Cv2.CV_SCHARR);
+            break;
+          case "Sharpen":
+            List<int> kernel = [-1, -1, -1, -1, 9, -1, -1, -1, -1];
+            imgLib.Image  __image = imgLib.copyCrop(_image, 0, 0, _image.width, _image.height);
+            __image = convolution(__image, kernel);
+            _byte = Uint8List.fromList(imgLib.encodeJpg(__image));
+            break;
+          case "OldTimeFilter":
+            imgLib.Image  __image = imgLib.copyCrop(_image, 0, 0, _image.width, _image.height);
+            for (int i = 0; i < __image.width; i++) {
+              for (int j = 0; j < __image.height; j++) {
+                var pixel = __image.getPixel(i, j);
+                int r = getR(pixel);
+                int g = getG(pixel);
+                int b = getB(pixel);
+                int newR = (0.393 * r + 0.769 * g + 0.189 * b).toInt();
+                int newG = (0.349 * r + 0.686 * g + 0.168 * b).toInt();
+                int newB = (0.272 * r + 0.534 * g + 0.131 * b).toInt();
+                pixel = setRGB(pixel, newR, newG, newB);
+                __image.setPixel(i, j, pixel);
+              }
+            }
+            _byte = Uint8List.fromList(imgLib.encodeJpg(__image));
             break;
           case "BlackWhite":
             _byte = await Cv2.threshold(
               pathFrom: CVPathFrom.GALLERY_CAMERA,
-              pathString: _image!.path,
+              pathString: _imagefile!.path,
               maxThresholdValue: 200,
               thresholdType: Cv2.THRESH_BINARY,
               thresholdValue: 150,
             );
             break;
-          case "invert":
+          case "RemoveColor":
+            _byte = await Cv2.cvtColor(
+                pathFrom: CVPathFrom.GALLERY_CAMERA,
+                pathString: _imagefile!.path,
+                outputType: Cv2.COLOR_BGR2GRAY);
+            break;
+          case "FusedFilter":
+            imgLib.Image  __image = imgLib.copyCrop(_image, 0, 0, _image.width, _image.height);
+            for (int i = 0; i < __image.width; i++) {
+              for (int j = 0; j < __image.height; j++) {
+                var pixel = __image.getPixel(i, j);
+                int r = getR(pixel);
+                int g = getG(pixel);
+                int b = getB(pixel);
+                int newR = ((r * 128) / (g + b + 1)).toInt();
+                int newG = ((g * 128) / (r + b + 1)).toInt();
+                int newB = ((b * 128) / (g + r + 1)).toInt();
+                pixel = setRGB(pixel, newR, newG, newB);
+                __image.setPixel(i, j, pixel);
+              }
+            }
+            _byte = Uint8List.fromList(imgLib.encodeJpg(__image));
+            break;
+          case "FreezeFilter":
+            imgLib.Image  __image = imgLib.copyCrop(_image, 0, 0, _image.width, _image.height);
+            for (int i = 0; i < __image.width; i++) {
+              for (int j = 0; j < __image.height; j++) {
+                var pixel = __image.getPixel(i, j);
+                int r = getR(pixel);
+                int g = getG(pixel);
+                int b = getB(pixel);
+                int newR = (((r - g - b) * 3) / 2).toInt();
+                int newG = (((g - r - b) * 3) / 2).toInt();
+                int newB = (((b - g - r) * 3) / 2).toInt();
+                pixel = setRGB(pixel, newR, newG, newB);
+                __image.setPixel(i, j, pixel);
+              }
+            }
+            _byte = Uint8List.fromList(imgLib.encodeJpg(__image));
+            break;
+          case "ComicstripFilter":
+            imgLib.Image  __image = imgLib.copyCrop(_image, 0, 0, _image.width, _image.height);
+            for (int i = 0; i < __image.width; i++) {
+              for (int j = 0; j < __image.height; j++) {
+                var pixel = __image.getPixel(i, j);
+                int r = getR(pixel);
+                int g = getG(pixel);
+                int b = getB(pixel);
+                int newR = (((g - b + g + r).abs() * r) / 256).toInt();
+                int newG = (((b - g + b + r).abs() * r) / 256).toInt();
+                int newB = (((b - g + b + r).abs() * g) / 256).toInt();
+                pixel = setRGB(pixel, newR, newG, newB);
+                __image.setPixel(i, j, pixel);
+              }
+            }
+            _byte = Uint8List.fromList(imgLib.encodeJpg(__image));
+            break;
+          case "Sketch":
+
+            break;
+          case "TestLutFilter":
+            imgLib.Image  __image = imgLib.copyCrop(_image, 0, 0, _image.width, _image.height);
+            for (int i = 0; i < __image.width; i++) {
+              for (int j = 0; j < __image.height; j++) {
+                var pixel = __image.getPixel(i, j);
+                int r = getR(pixel);
+                int g = getG(pixel);
+                int b = getB(pixel);
+                int lutX = (b % 8) * 64 + r;
+                int lutY = (b / 8).floor() * 64 + g;
+                // pixel = __image.getPixel(lutX, lutY);
+                __image.setPixel(i, j, pixel);
+              }
+            }
+            _byte = Uint8List.fromList(imgLib.encodeJpg(__image));
             break;
           default:
         }
@@ -201,11 +354,11 @@ class _FilterScreenState extends AppState<FilterScreen> {
                                   height: 300,
                                   fit: BoxFit.fill,
                                 )
-                              : _image != null
+                              : _imagefile != null
                                   ? Container(
                                       width: 300,
                                       height: 300,
-                                      child: Image.file(_image!),
+                                      child: Image.file(_imagefile!),
                                     )
                                   : Container(
                                       width: 300,
