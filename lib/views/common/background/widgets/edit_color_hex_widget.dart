3aa7992e0077e93b6c99a442661032eff6a0c271
王力 <2808699200@qq.com>
2023-08-18 10:22:21 +0800

    the color only refresh the bg

--- lib/views/common/background/widgets/edit_color_hex_widget.dart
+++ lib/views/common/background/widgets/edit_color_hex_widget.dart
@@ -16,8 +16,11 @@ class EditColorHexWidget extends StatefulWidget {
 }
 
 class _EditColorHexWidgetState extends State<EditColorHexWidget> {
-  _EditColorHexWidgetState(this.bgColor, this.colorHex);
+  _EditColorHexWidgetState(this.bgColor, this.colorHex) {
+    backgroundColor = bgColor.value.obs;
+  }
 
+  late RxInt backgroundColor;
   final textController = TextEditingController();
   Color bgColor;
   String colorHex;
@@ -25,14 +28,10 @@ class _EditColorHexWidgetState extends State<EditColorHexWidget> {
   final RegExp _colorRegExp = RegExp(r'^#(?:[0-9a-fA-F]{8})$'); // 正则表达式校验色值
 
   void _onColorChanged(String color) {
-    setState(() {
-      if (_colorRegExp.hasMatch(color)) {
-        textController.text = color.toUpperCase(); // 将输入的色值转换为大写形式
-      }
-      if (_colorRegExp.hasMatch(textController.text)) {
-        bgColor = ColorUtil.hexColor(textController.text);
-      }
-    });
+    if (_colorRegExp.hasMatch(textController.text)) {
+      bgColor = ColorUtil.hexColor(textController.text);
+      backgroundColor.value = bgColor.value;
+    }
   }
 
   @override
@@ -44,49 +43,59 @@ class _EditColorHexWidgetState extends State<EditColorHexWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
-      body: Column(
+      backgroundColor: Colors.transparent,
+      body: Stack(
         children: [
-          Image.asset(
-            Images.ic_bg_close,
-            width: $(24),
-          ).intoGestureDetector(onTap: () {
-            Navigator.of(context).pop();
-          }).intoPadding(padding: EdgeInsets.only(top: $(30), left: ScreenUtil.screenSize.width - $(39))),
-          Container(
-            margin: EdgeInsets.only(left: $(15), right: $(15), top: $(80)),
-            height: $(38),
-            alignment: Alignment.center,
-            child: TextField(
-                controller: textController,
-                textAlign: TextAlign.center,
-                onChanged: (color) {
-                  _onColorChanged(color);
-                },
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: $(18),
-                  fontWeight: FontWeight.bold,
+          Obx(
+                () =>
+                Container(
+                  color: Color(backgroundColor.value),
                 ),
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                )),
-            decoration: BoxDecoration(
-                color: Color(0xff444547),
-                borderRadius: BorderRadius.circular($(19)),
-                border: Border.all(
-                  color: Colors.white,
-                  width: 1,
-                )),
           ),
-          Expanded(child: SizedBox()),
-          TitleTextWidget(
-            S.of(context).submit,
-            Colors.white,
-            FontWeight.w400,
-            $(17),
-          )
-              .intoContainer(
+          Column(
+            children: [
+              Image.asset(
+                Images.ic_bg_close,
+                width: $(24),
+              ).intoGestureDetector(onTap: () {
+                Navigator.of(context).pop();
+              }).intoPadding(padding: EdgeInsets.only(top: $(30), left: ScreenUtil.screenSize.width - $(39))),
+              Container(
+                margin: EdgeInsets.only(left: $(15), right: $(15), top: $(80)),
+                height: $(38),
+                alignment: Alignment.center,
+                child: TextField(
+                    controller: textController,
+                    textAlign: TextAlign.center,
+                    onChanged: (color) {
+                      _onColorChanged(color);
+                    },
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: $(18),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    )),
+                decoration: BoxDecoration(
+                    color: Color(0xff444547),
+                    borderRadius: BorderRadius.circular($(19)),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 1,
+                    )),
+              ),
+              Expanded(child: SizedBox()),
+              TitleTextWidget(
+                S
+                    .of(context)
+                    .submit,
+                Colors.white,
+                FontWeight.w400,
+                $(17),
+              )
+                  .intoContainer(
                   alignment: Alignment.center,
                   height: $(38),
                   width: ScreenUtil.screenSize.width - $(30),
@@ -99,15 +108,19 @@ class _EditColorHexWidgetState extends State<EditColorHexWidget> {
                     ], begin: Alignment.topLeft, end: Alignment.bottomRight),
                     borderRadius: BorderRadius.circular($(19)),
                   ))
-              .intoGestureDetector(onTap: () {
-            if (_colorRegExp.hasMatch(textController.text)) {
-              widget.onColorSubmit.call(textController.text);
-              Navigator.of(context).pop();
-            } else {
-              CommonExtension().showToast(S.of(context).enter_the_color_value);
-              print("请输入正确的色值");
-            }
-          }),
+                  .intoGestureDetector(onTap: () {
+                if (_colorRegExp.hasMatch(textController.text)) {
+                  widget.onColorSubmit.call(textController.text);
+                  Navigator.of(context).pop();
+                } else {
+                  CommonExtension().showToast(S
+                      .of(context)
+                      .enter_the_color_value);
+                  print("请输入正确的色值");
+                }
+              }),
+            ],
+          ),
         ],
       ),
     );
