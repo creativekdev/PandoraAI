2ed554542a8200eb523e4869d48c8a322700f659
王力 <2808699200@qq.com>
2023-08-31 09:21:33 +0800

    signuped and return to origin screen

--- lib/Common/navigator_observer.dart
+++ lib/Common/navigator_observer.dart
@@ -1,21 +1,21 @@
 import 'package:cartoonizer/Common/importFile.dart';
 
 class AppRouteObserver extends NavigatorObserver {
-  List<Route<dynamic>> _routeHistory = [];
+  List<Route<dynamic>> routeHistory = [];
   Route<dynamic>? _currentRoute;
 
   Route<dynamic>? get currentRoute => _currentRoute;
 
   Route<dynamic>? get lastRoute {
-    if (_routeHistory.length >= 2) {
-      return _routeHistory[_routeHistory.length - 2];
+    if (routeHistory.length >= 2) {
+      return routeHistory[routeHistory.length - 2];
     }
     return null;
   }
 
   bool isContainRoute(String route) {
-    for (var i = 0; i < _routeHistory.length; i++) {
-      if (_routeHistory[i].settings.name == route) {
+    for (var i = 0; i < routeHistory.length; i++) {
+      if (routeHistory[i].settings.name == route) {
         return true;
       }
     }
@@ -25,14 +25,14 @@ class AppRouteObserver extends NavigatorObserver {
   @override
   void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPush(route, previousRoute);
-    _routeHistory.add(route);
+    routeHistory.add(route);
     _currentRoute = route;
   }
 
   @override
   void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPop(route, previousRoute);
-    _routeHistory.remove(route);
+    routeHistory.remove(route);
     _currentRoute = previousRoute;
   }
 }
